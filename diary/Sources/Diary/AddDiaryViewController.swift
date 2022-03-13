c2075379af2ac01c999f6a38ae438021cc85f783
Chae Haram <djvemfla2@gmail.com>
2022-03-14 01:19:26 +0900

    edit image update

--- diary/Sources/Diary/AddDiaryViewController.swift
+++ diary/Sources/Diary/AddDiaryViewController.swift
@@ -109,7 +109,7 @@ extension AddDiaryViewController: UIImagePickerControllerDelegate & UINavigation
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             addDiaryImageView.image = image
-            Diary.diaryList[row].picture = image!
+            Diary.diaryList[row!].picture = image
         } else {
             print("이미지 선택 실패")
         }
