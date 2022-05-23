336fa3f2e6069d8cc85825b774099263be5e3916
Chae Haram <djvemfla2@gmail.com>
2022-05-24 00:38:25 +0900

    append date

--- GraphViewController.swift
+++ GraphViewController.swift
@@ -21,12 +21,14 @@ class GraphViewController: UIViewController {
     var checkCount: [Double] = [8,5,4,2,1,2,3]
     var strDates: [String] = []
     var todayDate: Date = Date()
+    var monthInfo: Int = 0
+    var weekInfo: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        appendDate(date: todayDate)
         setCalendar()
-        drawGraph()
         
         calendarView.reloadData()
     }
@@ -38,65 +40,72 @@ class GraphViewController: UIViewController {
     }
     
     func appendDate(date: Date) {
-        
-    }
-    
-    func setCalendar() {
-        calendarView.delegate = self
-        calendarView.dataSource = self
-        
-        let monthInfo = Calendar.current.component(.month, from: todayDate)
-        let weekInfo = Calendar.current.component(.weekOfMonth, from: todayDate)
+        strDates = []
+        monthInfo = Calendar.current.component(.month, from: date)
+        weekInfo = Calendar.current.component(.weekOfMonth, from: date)
         calendarInfo.text = "\(monthInfo)월 \(weekInfo)째주"
-        if Calendar.current.component(.weekday, from: todayDate) == 1 {
+        if Calendar.current.component(.weekday, from: date) == 1 {
             for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: todayDate)!
+                let addDate = Calendar.current.date(byAdding: .day, value: i, to: date)!
                 let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
                 strDates.append(dateToStr)
             }
-        } else if Calendar.current.component(.weekday, from: todayDate) == 2 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -1, to: todayDate)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-        } else if Calendar.current.component(.weekday, from: todayDate) == 3 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -2, to: todayDate)!
+        } else if Calendar.current.component(.weekday, from: date) == 2 {
+            let editDate = Calendar.current.date(byAdding: .day, value: -1, to: date)!
             for i in 0...7 {
                 let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
                 let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
                 strDates.append(dateToStr)
             }
-        } else if Calendar.current.component(.weekday, from: todayDate) == 4 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -3, to: todayDate)!
+        } else if Calendar.current.component(.weekday, from: date) == 3 {
+            let editDate = Calendar.current.date(byAdding: .day, value: -2, to: date)!
             for i in 0...7 {
                 let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
                 let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
                 strDates.append(dateToStr)
             }
-        } else if Calendar.current.component(.weekday, from: todayDate) == 5 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -4, to: todayDate)!
+        } else if Calendar.current.component(.weekday, from: date) == 4 {
+            let editDate = Calendar.current.date(byAdding: .day, value: -3, to: date)!
             for i in 0...7 {
                 let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
                 let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
                 strDates.append(dateToStr)
             }
-        } else if Calendar.current.component(.weekday, from: todayDate) == 6 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -5, to: todayDate)!
+        } else if Calendar.current.component(.weekday, from: date) == 5 {
+            let editDate = Calendar.current.date(byAdding: .day, value: -4, to: date)!
             for i in 0...7 {
                 let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
                 let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
                 strDates.append(dateToStr)
             }
-        } else if Calendar.current.component(.weekday, from: todayDate) == 7 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -6, to: todayDate)!
+        } else if Calendar.current.component(.weekday, from: date) == 6 {
+            let editDate = Calendar.current.date(byAdding: .day, value: -5, to: date)!
+            for i in 0...7 {
+                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
+                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
+                strDates.append(dateToStr)
+            }
+        } else if Calendar.current.component(.weekday, from: date) == 7 {
+            let editDate = Calendar.current.date(byAdding: .day, value: -6, to: date)!
             for i in 0...7 {
                 let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
                 let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
                 strDates.append(dateToStr)
             }
         }
+        drawGraph()
+        appendData(datas: strDates)
+    }
+    
+    func appendData(datas: [String]) {
+        for data in datas {
+            if data == MyDB.toDoList
+        }
+    }
+    
+    func setCalendar() {
+        calendarView.delegate = self
+        calendarView.dataSource = self
         
         calendarView.locale = Locale(identifier: "ko-KR")
         calendarView.appearance.selectionColor = .systemBlue
@@ -124,10 +133,7 @@ class GraphViewController: UIViewController {
         
         barChart.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
         
-        
     }
-   
-
 }
 
 extension GraphViewController: FSCalendarDelegate, FSCalendarDataSource {
@@ -155,69 +161,8 @@ extension GraphViewController: FSCalendarDelegate, FSCalendarDataSource {
         calendarList = MyDB.toDoList.filter { toDo in
             toDo.startDate == date
         }
-        strDates = []
-        let monthInfo = Calendar.current.component(.month, from: date)
-        let weekInfo = Calendar.current.component(.weekOfMonth, from: date)
-        calendarInfo.text = "\(monthInfo)월 \(weekInfo)째주"
-        if Calendar.current.component(.weekday, from: date) == 1 {
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: date)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
-        } else if Calendar.current.component(.weekday, from: date) == 2 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -1, to: date)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
-        } else if Calendar.current.component(.weekday, from: date) == 3 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -2, to: date)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
-        } else if Calendar.current.component(.weekday, from: date) == 4 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -3, to: date)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
-        } else if Calendar.current.component(.weekday, from: date) == 5 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -4, to: date)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
-        } else if Calendar.current.component(.weekday, from: date) == 6 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -5, to: date)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
-        } else if Calendar.current.component(.weekday, from: date) == 7 {
-            let editDate = Calendar.current.date(byAdding: .day, value: -6, to: date)!
-            for i in 0...7 {
-                let addDate = Calendar.current.date(byAdding: .day, value: i, to: editDate)!
-                let dateToStr = DateFormatter.customDateFormatter.dayToStr(date: addDate)
-                strDates.append(dateToStr)
-            }
-            drawGraph()
+        if Calendar.current.component(.weekOfMonth, from: date) != weekInfo {
+            appendDate(date: date)
         }
-        
-//        if calendarList.count == 0 {
-//            UIAlertController.showAlert(message: "등록된 투두가 없습니다.", vc: self)
-//        }
     }
 }
