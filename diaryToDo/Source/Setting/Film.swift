511fb3427bed49becc58f1f1d8243deb0c77f3ec
Chae Haram <djvemfla2@gmail.com>
2022-08-09 18:31:06 +0900

    주석 제거

--- diaryToDo/Source/Setting/Film.swift
+++ diaryToDo/Source/Setting/Film.swift
@@ -7,18 +7,6 @@
 
 import UIKit
 
-//struct Film {
-//    var filmType: UIImage
-//    var isSelectType: Bool = false
-//
-//    static var filmList: [Film] = [
-//        Film(filmType: UIImage(named: "film1")!),
-//        Film(filmType: UIImage(named: "film2")!),
-//        Film(filmType: UIImage(named: "film3")!),
-//        Film(filmType: UIImage(named: "film4")!)
-//    ]
-//}
-
 enum FilmType: String {
     case film1
     case film2
