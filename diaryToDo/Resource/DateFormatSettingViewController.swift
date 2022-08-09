7a61a8bd9a2eb359e96a142bd206929ac4e96361
Chae Haram <djvemfla2@gmail.com>
2022-08-09 18:29:11 +0900

    날자 형식 설정

--- diaryToDo/Resource/DateFormatSettingViewController.swift
+++ diaryToDo/Resource/DateFormatSettingViewController.swift
@@ -13,55 +13,70 @@ class DateFormatSettingViewController: UIViewController {
     
     @IBOutlet weak var dateFormatTableView: UITableView!
     
+    var selectedDateFormat: DateFormatType = .type1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dateFormatTableView.dataSource = self
         dateFormatTableView.delegate = self
-        dateFormatTableView.reloadData()
         
         let rightDoneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(setDoneButton))
         self.navigationItem.rightBarButtonItem = rightDoneButton
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        dateFormatTableView.reloadData()
+    func setDateStyleSelect(_ sender: UIButton) {
+        sender.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+    }
+    
+    func setDateStyleNotSelect(_ sender: UIButton) {
+        sender.setImage(UIImage(systemName: "circle"), for: .normal)
     }
     
     @objc func setDoneButton() {
+        
         self.navigationController?.popViewController(animated: true)
     }
     
     @objc func isSelectDateFormat(_ sender: UIButton) {
-        if sender.isSelected {
-            sender.setImage(UIImage(systemName: "circle"),for: .normal)
-            sender.isSelected = false
-            DateFormat.dateFormatList[sender.tag].isSelectType = false
-        } else {
-            sender.setImage(UIImage(systemName: "circle.fill"), for: .normal)
-            sender.isSelected = true
-            DateFormat.dateFormatList[sender.tag].isSelectType = true
+        for i in 0..<MyDB.dateFormatList.count {
+            if sender.tag == i {
+                if !MyDB.dateFormatList[i].isSelected {
+                    MyDB.dateFormatList[i].isSelected.toggle()
+                }
+            } else {
+                MyDB.dateFormatList[i].isSelected = false
+            }
         }
+        dateFormatTableView.reloadData()
     }
 }
 
 extension DateFormatSettingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return DateFormat.dateFormatList.count
+        return MyDB.dateFormatList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = dateFormatTableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath) as? DateFormatTableViewCell else { return UITableViewCell() }
-        let dateFormat = DateFormat.dateFormatList[indexPath.row]
+        let dateFormat = MyDB.dateFormatList[indexPath.row]
         
-        cell.typeDateFormat.text = dateFormat.typeDateFormat
+        if dateFormat.isSelected {
+            setDateStyleSelect(cell.selectedDateFormat)
+        } else {
+            setDateStyleNotSelect(cell.selectedDateFormat)
+        }
+        
+        cell.typeDateFormat.text = dateFormat.dateformatType.rawValue
         cell.selectedDateFormat.tag = indexPath.row
         cell.selectedDateFormat.addTarget(self, action: #selector(isSelectDateFormat), for: .touchUpInside)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
 
