757a8cf466e6e2180b33ffb2b0472dab3dfa514b
Chae Haram <djvemfla2@gmail.com>
2022-08-11 00:28:37 +0900

    필름 이미지 세팅 완료

--- diaryToDo/Source/Setting/FilmSettingViewController.swift
+++ diaryToDo/Source/Setting/FilmSettingViewController.swift
@@ -9,15 +9,14 @@ import UIKit
 
 /*
  TODO:
- - cell 눌렀을 때 토글되게 하기
- - '완료'버튼 눌렀을 때 저장되어있게 하기
- - 만약 저장하지 않고 뒤로가면, 저장하기 전에 선택했던 필름으로 설정
+ - 바뀐 필름 적용하기 -> 
  */
 
 class FilmSettingViewController: UIViewController {
     
     @IBOutlet weak var filmTableView: UITableView!
     
+    var filmList = MyDB.filmList
     var selectedFilm: FilmType = .film1
     
     override func viewDidLoad() {
@@ -28,6 +27,19 @@ class FilmSettingViewController: UIViewController {
         
         let rightDoneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(setDoneButton))
         self.navigationItem.rightBarButtonItem = rightDoneButton
+    }
+    
+    func toggleAndReload(index: Int) {
+        for i in 0..<filmList.count {
+            if index == i {
+                if !filmList[i].isSelected {
+                    filmList[i].isSelected.toggle()
+                }
+            } else {
+                filmList[i].isSelected = false
+            }
+        }
+        filmTableView.reloadData()
     }
     
     func toImage(filmType: FilmType) -> UIImage {
@@ -44,32 +56,23 @@ class FilmSettingViewController: UIViewController {
     }
     
     @objc func setDoneButton() {
+        MyDB.filmList = filmList
         self.navigationController?.popViewController(animated: true)
     }
     
     @objc func isSelectFilm(_ sender: UIButton) {
-        for i in 0..<MyDB.filmList.count {
-            if sender.tag == i {
-                if !MyDB.filmList[i].isSelected {
-                    MyDB.filmList[i].isSelected.toggle()
-                }
-            } else {
-                MyDB.filmList[i].isSelected = false
-            }
-        }
-    
-        filmTableView.reloadData()
+        toggleAndReload(index: sender.tag)
     }
 }
 
 extension FilmSettingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return MyDB.filmList.count
+        return filmList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = filmTableView.dequeueReusableCell(withIdentifier: "filmCell", for: indexPath) as? FilmTableViewCell else { return UITableViewCell() }
-        let selectFilm = MyDB.filmList[indexPath.row]
+        let selectFilm = filmList[indexPath.row]
         
         if selectFilm.isSelected {
             setImageSelect(cell.isSelectFilmButton)
@@ -87,12 +90,7 @@ extension FilmSettingViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //TODO: Cell을 누를때 선택되도록 수정해보기!
-        let selectCell = MyDB.filmList[indexPath.row]
-                
-        
-
-        print(selectCell.filmName)
+        toggleAndReload(index: indexPath.row)
     }
 }
 
