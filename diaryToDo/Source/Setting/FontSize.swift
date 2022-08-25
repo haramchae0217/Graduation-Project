ca1b157fc855be2a5daf3efca0da1c271eb37a82
Chae Haram <djvemfla2@gmail.com>
2022-08-26 00:52:14 +0900

    기본 글씨크기 수정

--- diaryToDo/Source/Setting/FontSize.swift
+++ diaryToDo/Source/Setting/FontSize.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 enum FontSize: CGFloat {
-    case 아주작게 = 10
-    case 작게 = 12
-    case 중간 = 14
-    case 크게 = 16
-    case 아주크게 = 18
+    case 아주작게 = 18
+    case 작게 = 20
+    case 중간 = 22
+    case 크게 = 24
+    case 아주크게 = 26
 }
