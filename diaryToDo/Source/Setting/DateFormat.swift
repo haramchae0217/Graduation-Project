c9fc6525689beeae418507402d671c9f488b424a
Chae Haram <djvemfla2@gmail.com>
2022-08-25 23:19:43 +0900

    날짜형식 변경

--- diaryToDo/Source/Setting/DateFormat.swift
+++ diaryToDo/Source/Setting/DateFormat.swift
@@ -10,6 +10,7 @@ import UIKit
 enum DateFormatType: String {
     case type1 = "2022.01.24(월)"
     case type2 = "Mon 24 Jan 2022"
-    case type3 = "2022년 01월 24일"
-    case type4 = "2022-01-24"
+    case type3 = "2022년 01월 24일 월요일"
+    case type4 = "2022-01-24-월"
+    case type5 = "2022/01/24/월"
 }
