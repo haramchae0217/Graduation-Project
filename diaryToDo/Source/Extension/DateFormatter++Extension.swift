644bc6b99793b4a72671596ec05bbf38ca944029
Chae Haram <djvemfla2@gmail.com>
2022-08-26 00:52:46 +0900

    날짜형식 수정

--- diaryToDo/Source/Extension/DateFormatter++Extension.swift
+++ diaryToDo/Source/Extension/DateFormatter++Extension.swift
@@ -11,7 +11,7 @@ extension DateFormatter {
     static let customDateFormatter: DateFormatter = {
         let df = DateFormatter()
         df.dateFormat = "yyyy/MM/dd"
-        df.locale = Locale(identifier: "ko_KR")
+//        df.locale = Locale(identifier: "ko_KR")
         return df
     }()
     
@@ -23,15 +23,15 @@ extension DateFormatter {
     func dateToStr(date: Date, type: DateFormatType) -> String {
         switch type {
         case .type1:
-            self.dateFormat = "yyyy.MM.dd(E)"
+            self.dateFormat = "dd MMM, yyyy"
         case .type2:
-            self.dateFormat = "EEE dd MMM yyyy"
+            self.dateFormat = "yyyy.MM.dd"
         case .type3:
-            self.dateFormat = "yyyy년 MM월 dd일 EEE"
+            self.dateFormat = "yyyy년 MM월 dd일"
         case .type4:
-            self.dateFormat = "yyyy-MM-dd-E"
+            self.dateFormat = "yyyy-MM-dd"
         case .type5:
-            self.dateFormat = "yyyy/MM/dd/E"
+            self.dateFormat = "yyyy/MM/dd"
         }
         return self.string(from: date)
     }
