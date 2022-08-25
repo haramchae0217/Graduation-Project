2c901a0ebb8af26edc77c57e52c9479924977052
Chae Haram <djvemfla2@gmail.com>
2022-08-25 23:20:03 +0900

    글꼴 이름 변경 및 추가

--- diaryToDo/Source/Setting/Font.swift
+++ diaryToDo/Source/Setting/Font.swift
@@ -8,11 +8,29 @@
 import Foundation
 
 enum FontName: String {
-    case name1 = "Apple SD 산돌고딕 Neo"
-    case name2 = "Apple Gothic"
-    case name3 = "Apple Myungjo"
-    case name4 = "D2 Coding"
-    case name5 = "D2 Coding ligature"
-    case name6 = "GungSeo"
-    case name7 = "HeadLine A"
+    case name1 = "Ownglyph ssojji" // o
+    case name2 = "온글잎 보현체" // o
+    case name3 = "Ownglyph wiri" // o
+    case name4 = "Ownglyph Ahee" // o
+    case name5 = "온글잎 윤우체" // o
+    case name6 = "온글잎 의연체" // o
+    case name7 = "Ownglyph Kjh1030" // o
+    case name8 = "Ownglyph ZZANGO903" // o
+    case name9 = "Ownglyph JourneyofYouth" // o
+    case name10 = "Chilgok_Cye" // o
+    case name11 = "Chilgok_Kaj" // o
+    case name12 = "Chilgok_kyb" // o
+    case name13 = "Chilgok_ljh" // o
+    case name14 = "Chilgok_lws" // o
+    case name15 = "Chosun Centennial" // o
+    case name16 = "조선굴림체" // o
+    case name17 = "HSGaeulSenggak2.0" // o
+    case name18 = "HS유지체" //o
+    case name19 = "HSBombaram3.0" // o
+    case name20 = "HS겨울눈꽃체2.0" // o
+    case name21 = "HS새마을체" //o
+    case name22 = "HS산토끼체" //o
+    case name23 = "JeonHwaseon" // o
+    case name24 = "LeeSeoyun" // o
+    case name25 = "SimKyungha" // o
 }
