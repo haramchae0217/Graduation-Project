6b1647257b61fba84ed78bc9bdb8c0e8b5a94c13
Chae Haram <djvemfla2@gmail.com>
2022-08-25 23:19:19 +0900

    익스텐션 날짜형식 타입 설정

--- diaryToDo/Source/Extension/DateFormatter++Extension.swift
+++ diaryToDo/Source/Extension/DateFormatter++Extension.swift
@@ -20,8 +20,19 @@ extension DateFormatter {
         return self.date(from: str)!
     }
     
-    func dateToStr(date: Date) -> String {
-        self.dateFormat = "MMM d, yyyy"
+    func dateToStr(date: Date, type: DateFormatType) -> String {
+        switch type {
+        case .type1:
+            self.dateFormat = "yyyy.MM.dd(E)"
+        case .type2:
+            self.dateFormat = "EEE dd MMM yyyy"
+        case .type3:
+            self.dateFormat = "yyyy년 MM월 dd일 EEE"
+        case .type4:
+            self.dateFormat = "yyyy-MM-dd-E"
+        case .type5:
+            self.dateFormat = "yyyy/MM/dd/E"
+        }
         return self.string(from: date)
     }
     
@@ -29,7 +40,7 @@ extension DateFormatter {
         self.dateFormat = "dd"
         return self.date(from: str)!
     }
-    
+
     func dayToStr(date: Date) -> String {
         self.dateFormat = "dd"
         return self.string(from: date)
