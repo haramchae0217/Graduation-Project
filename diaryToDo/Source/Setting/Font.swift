5b9438fb2544409a3ff01b764ae73df29fbeeaf4
Chae Haram <djvemfla2@gmail.com>
2022-08-26 23:03:17 +0900

    주석 삭제

--- diaryToDo/Source/Setting/Font.swift
+++ diaryToDo/Source/Setting/Font.swift
@@ -8,29 +8,29 @@
 import Foundation
 
 enum FontName: String {
-    case name1 = "Ownglyph ssojji" //oo
-    case name2 = "온글잎 보현체" //
-    case name3 = "Ownglyph wiri" // oo
-    case name4 = "Ownglyph Ahee" // oo
-    case name5 = "온글잎 윤우체" //
-    case name6 = "온글잎 의연체" //
-    case name7 = "Ownglyph Kjh1030" // oo
-    case name8 = "Ownglyph ZZANG0903" // oo
-    case name9 = "Ownglyph JourneyofYouth" // oo
-    case name10 = "Chilgok_Cye" // oo
-    case name11 = "Chilgok_Kaj" // oo
-    case name12 = "Chilgok_kyb" // oo
-    case name13 = "Chilgok_ljh" // oo
-    case name14 = "Chilgok_lws" // oo
-    case name15 = "Chosun Centennial" // oo
-    case name16 = "조선굴림체" // oo
-    case name17 = "HSGaeulSenggak 2.0" // oo
-    case name18 = "HS유지체" //oo
-    case name19 = "HSBomBaram 3.0" // oo
-    case name20 = "HS겨울눈꽃체2.0" // oo
-    case name21 = "HS새마을체" //oo
-    case name22 = "HS산토끼체" //oo
-    case name23 = "JeonHwaseon" // oo
-    case name24 = "LeeSeoyun" // oo
-    case name25 = "SimKyungha" // oo
+    case name1 = "Ownglyph ssojji"
+    case name2 = "온글잎 보현체"
+    case name3 = "Ownglyph wiri"
+    case name4 = "Ownglyph Ahee"
+    case name5 = "온글잎 윤우체"
+    case name6 = "온글잎 의연체"
+    case name7 = "Ownglyph Kjh1030"
+    case name8 = "Ownglyph ZZANG0903"
+    case name9 = "Ownglyph JourneyofYouth"
+    case name10 = "Chilgok_Cye"
+    case name11 = "Chilgok_Kaj"
+    case name12 = "Chilgok_kyb"
+    case name13 = "Chilgok_ljh"
+    case name14 = "Chilgok_lws"
+    case name15 = "Chosun Centennial"
+    case name16 = "조선굴림체"
+    case name17 = "HSGaeulSenggak 2.0"
+    case name18 = "HS유지체"
+    case name19 = "HSBomBaram 3.0"
+    case name20 = "HS겨울눈꽃체2.0"
+    case name21 = "HS새마을체"
+    case name22 = "HS산토끼체"
+    case name23 = "JeonHwaseon"
+    case name24 = "LeeSeoyun"
+    case name25 = "SimKyungha"
 }
