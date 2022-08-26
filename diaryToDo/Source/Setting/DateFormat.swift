73b117b73b4e7dee6fdac8c066cda138dfcfcd55
Chae Haram <djvemfla2@gmail.com>
2022-08-26 23:03:29 +0900

    이넘타입 수정

--- diaryToDo/Source/Setting/DateFormat.swift
+++ diaryToDo/Source/Setting/DateFormat.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 enum DateFormatType: String {
-    case type1 = "24 Jan, 2022"
-    case type2 = "2022.01.24"
-    case type3 = "2022년 01월 24일"
-    case type4 = "2022-01-24"
-    case type5 = "2022/01/24"
+    case type1
+    case type2
+    case type3
+    case type4
+    case type5
 }
