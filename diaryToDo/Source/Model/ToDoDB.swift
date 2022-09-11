a01a960783e9fa1a917447cc4c615574c78bc4e0
Chae Haram <djvemfla2@gmail.com>
2022-09-12 00:07:57 +0900

    새로운 투두 클래스 생성

---
+++ diaryToDo/Source/Model/ToDoDB.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoDB.swift
+//  diaryToDo
+//
+//  Created by Chae_Haram on 2022/09/12.
+//
+
+import UIKit
+import RealmSwift
+
+class ToDoDB: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var title: String
+    @Persisted var memo: String
+    @Persisted var startDate: Date
+    @Persisted var endDate: Date
+    @Persisted var isChecked: Bool = false
+    
+    convenience init(title: String, memo: String, startDate: Date, endDate: Date) {
+        self.init()
+        self.title = title
+        self.memo = memo
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+}
+
+class SelectToDo: ToDoDB {
+    
+}
