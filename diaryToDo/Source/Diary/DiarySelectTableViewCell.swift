fa463ba9499345b99cd1e6e18e6022c2c0386cfb
Chae Haram <djvemfla2@gmail.com>
2022-09-20 18:25:48 +0900

    다이어리 테이블뷰 셀 아울렛 연결

---
+++ diaryToDo/Source/Diary/DiarySelectTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DiarySelectTableViewCell.swift
+//  diaryToDo
+//
+//  Created by Chae_Haram on 2022/09/20.
+//
+
+import UIKit
+
+class DiarySelectTableViewCell: UITableViewCell {
+    
+    static let identifier = "SelectDiaryCell"
+
+    @IBOutlet weak var selectDiaryUIImage: UIImageView!
+    @IBOutlet weak var selectDiaryHashTag: UILabel!
+    @IBOutlet weak var selectDiaryDate: UILabel!
+
+}
